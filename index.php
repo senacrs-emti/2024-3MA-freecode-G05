c8c1c771a5b75ed52dc9db56a176db9b99d58c6b
laurooo <108877034+laurooo@users.noreply.github.com>
2024-11-01 12:25:48 -0300

    fazendo a tabela de classificação

--- index.php
+++ index.php
@@ -5,49 +5,47 @@ include './header/header.php'; // Inclui o header
 ?>
 <section>
     <!-- Tabela da Esquerda com barra de rolagem na esquerda -->
-    <div class="partidas">
+    <div class="partidas left">
         <div class="scroll-container left-scroll">
+            <!-- Conteúdo da tabela da esquerda -->
             <div class="barra">
+                <img class="times" src="./img/gremio.png" alt=""><p>gre</p>
+                <p>X</p>
+                <p>int</p><img class="times" src="./img/inter.png" alt="">
+            </div>
+            <div class="barra">
+                <img class="times" src="./img/fla.png" alt=""><p>fla</p>
+                <p>X</p>
+                <p>flu</p><img class="times" src="./img/flu.png" alt="">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
+            </div><div class="barra">
                 <img class="times" src="gremio.png" alt=""><p>gre</p>
                 <p>X</p>
                 <p>int</p><img class="times" src="inter.png" alt="">
@@ -56,45 +54,27 @@ include './header/header.php'; // Inclui o header
     </div>
 
     <!-- Tabela da Direita com barra de rolagem na direita -->
-    <div class="partidas">
-        <div class="scroll-container right-scroll">
-            <div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
-            <div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div><div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div><div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div><div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div><div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div><div class="barra">
-                <img class="times" src="gremio.png" alt=""><p>gre</p>
-                <p>X</p>
-                <p>int</p><img class="times" src="inter.png" alt="">
-            </div>
+    <div class="partidas right">
+    <div class="">TABELA DE CLASSIFICAÇÃO</div>
+    <div class="scroll-container right-scroll">
+        <!-- Conteúdo da tabela da direita -->
+        <div class="tabela">
+            <img class="times" src="./img/BOT.png" alt=""><p></p>
+        </div>
+        <div class="tabela">
+            <img class="times" src="./img/PAL.png" alt=""><p>pal</p>
+        </div>
+        <div class="tabela">
+            <img class="times" src="./img/FOR.png" alt=""><p>for</p>
+        </div>
+        <div class="tabela">
+            <img class="times" src="./img/FLA.png" alt=""><p>fla</p>
+        </div>
+        <div class="tabela">
+            <img class="times" src="./img/inter.png" alt=""><p>int</p>
         </div>
     </div>
+</div>
 </section>
 
 
