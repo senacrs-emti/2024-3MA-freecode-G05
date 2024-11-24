f6c6427ee72ee4a67e7776be6429c2accc7dd9a6
viniross <108466053+viniross@users.noreply.github.com>
2024-11-24 19:18:20 -0300

    Registering user, email and password in db.

--- database/conexao.php
+++ database/conexao.php
@@ -5,9 +5,9 @@ $username = "root";
 $password = "";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
+/*Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Connected successfully";*/
 ?>
--- login/login.php
+++ login/login.php
@@ -2,6 +2,24 @@
 include_once '../configuracao.php';
 include_once '../header/header.php'; 
 include_once '../database/conexao.php';
+
+    if (isset($_POST['submit'])) {
+        /*print_r('Usuário: ' . $_POST['usuario']);
+        print_r('<br>');
+        print_r('Email: ' .$_POST['email']);
+        print_r($_POST['nome']);*/
+
+        $user = $_POST['usuario'];
+        $email = $_POST['email'];
+        $password = $_POST['senha'];
+
+        $query = "INSERT INTO avaliacao(nota, comentario) VALUES (NULL, NULL)";
+        mysqli_query($conn, $query);
+        $avaliacao_id = mysqli_insert_id($conn);
+        
+        $result = mysqli_query($conn, "INSERT INTO login(user, email, senha, avaliacao_idavaliacao) VALUES ('$user', '$email', '$password', '$avaliacao_id')");        
+
+    }
 ?>
 
     <div class="container">
@@ -15,12 +33,12 @@ include_once '../database/conexao.php';
 
                 <div class="login">
                     <h2 class="title">Login</h2>
-                    <form class="fLogin">
+                    <form action="login.php" method="post" class="fLogin">
                         <label for="flUsuario">Usuário:</label>
-                        <input type="text" placeholder = "Digite seu Usuário">
+                        <input type="text" name = "usuario" placeholder = "Digite seu Usuário" required>
                         <label for="flSenha">Senha:</label>
-                        <input type="password" placeholder = "Digite sua Senha">
-                        <button class = "btn btn-entrar">Entrar</button>
+                        <input type="password" name = "senha" placeholder = "Digite sua Senha" required>
+                        <button type = "submit" name = "submit" class = "btn btn-entrar">Entrar</button>
                     </form>
                     <p>Ainda não tem conta?<a id="btnCadastro" href="#"><u>Cadastre-se</u></a></p>
                 </div>
@@ -34,14 +52,14 @@ include_once '../database/conexao.php';
 
                 <div class="cadastro">
                     <h2 class="title">Cadastre-se</h2>
-                    <form class="fCadastro">
+                    <form action="login.php" method="post" class="fCadastro" required>
                         <label for="fcEmail">Email:</label>
-                        <input type="email" placeholder = "Digite seu Email">
+                        <input type="email" name = "email" placeholder = "Digite seu Email" required>
                         <label for="fcUsuario">Usuário:</label>
-                        <input type="text" placeholder = "Digite seu Usuário">
+                        <input type="text" name = "usuario" placeholder = "Digite seu Usuário" required>
                         <label for="fcSenha">Senha:</label>
-                        <input type="password" placeholder = "Digite sua Senha">
-                        <button class = "btn btn-cadastrar">Cadastrar</button>
+                        <input type="password" name = "senha" placeholder = "Digite sua Senha" required>
+                        <button type = "submit" name = "submit" class = "btn btn-cadastrar">Cadastrar</button>
                     </form>
                     <p>Já tem conta?<a id="btnLogin" href="#"><u>Entre</u></a></p>
                 </div>
