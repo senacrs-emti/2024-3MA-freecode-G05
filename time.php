aea29491869b600c464b3cd2bf25879eeaf74df1
Artur Teixeira dos Santos <artur2006santos@gmail.com>
2024-11-27 10:59:51 -0300

    pagina do time

---
+++ time.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Times</title>
+    <link rel="stylesheet" href="time.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <img src="img/gremio.png">
+        <h1>Grêmio Foot-ball Porto Alegrense</h1>
+        <img src="img/mascotes/gremio.png">
+    </header>
+</body>
+</html>
+<?php
+include_once '../footer/footer.php';
+?>
