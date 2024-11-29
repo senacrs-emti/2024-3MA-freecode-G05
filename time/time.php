3732567a5aaaa3b391d62558f479e960cd87cfd3
Artur Teixeira dos Santos <artur2006santos@gmail.com>
2024-11-29 09:24:12 -0300

    Finalizando página dos times

--- time/time.php
+++ time/time.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Times</title>
-    <link rel="stylesheet" href="time.css">
+    <link rel="stylesheet" href="times.css">
+    <link rel="stylesheet" href="../footer/baixo.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
@@ -15,8 +16,22 @@
         <h1>Grêmio</h1>
         <img src="../img/mascotes/gremio.png">
     </header>
+    <div class="barra">
+    <div class="times-container">
+        <img class="times" src="../img/gremio.png" alt=""><p>GRE</p>
+        <p>X</p>
+        <p>INT</p><img class="times" src="../img/inter.png" alt="">
+    </div>
+    <div class="info">
+        <p>Local: Arena do Grêmio</p>
+        <p>Data: 03/11/2024</p>
+        <p>Horário: 16:00</p>
+    </div>
+</div>
+        <footer>
+        <?php
+            include_once '../footer/footer.php';
+        ?>
+        </footer>
 </body>
 </html>
-<?php
-include_once '../footer/footer.php';
-?>
