2a957d2c49f160efff5c6b9b9aa2ac2e6b122e1a
Artur Teixeira dos Santos <artur2006santos@gmail.com>
2024-12-06 10:08:27 -0300

    Atualização Tabela e Próximas Partida

--- main/index.php
+++ main/index.php
@@ -3,18 +3,17 @@ include_once '../database/conexao.php';
 include_once '../configuracao.php'; 
 include_once '../header/header.php'; 
 
-//TEM Q FAZER INNER JOIN
-// comando sql para capturar os dados
+// Comando SQL para capturar os dados
 $sql = "SELECT
         p.idpartidas,
         p.data,
         p.estadio,
         p.idtimeCasa,
         p.idtimeVis,
-        tc.nome As NomeTimeCasa,
+        tc.nome AS NomeTimeCasa,
         tc.escudo AS EscudoTimeCasa,
         tc.sigla AS SiglaTimeCasa,
-        tv.nome As NomeTimeVis,
+        tv.nome AS NomeTimeVis,
         tv.escudo AS EscudoTimeVis,
         tv.sigla AS SiglaTimeVis
         FROM partidas AS p
@@ -22,11 +21,9 @@ $sql = "SELECT
         ON p.idtimeCasa = tc.idtime
         INNER JOIN times AS tv
         ON p.idtimeVis = tv.idtime;";
-// executa o comando
-$result = mysqli_query( $conn, $sql );
-// transforma o resultado em dados
-$data = mysqli_fetch_assoc($result);
-// Conteúdo da página principal
+
+// Executa o comando
+$result = mysqli_query($conn, $sql);
 ?>
 
 <section>
@@ -36,23 +33,24 @@ $data = mysqli_fetch_assoc($result);
         <div class="titulo-classificacao">PRÓXIMAS PARTIDAS</div>
         <?php
         if (mysqli_num_rows($result) > 0) {
-            // output data of each row
+            // Percorre cada partida e exibe os dados
             while($row = mysqli_fetch_assoc($result)) {
-            //echo "Escudo: " . $row["EscudoTimeCasa"]. " - Sigla: " . $row["SiglaTimeCasa"]. "X" ."Sigla" . $row["SiglaTimeVis"]. "Escudo". $row["EscudoTimeVis"]."<br>";
             ?>
             <a class="barra" href="../partida/partida.php?p=<?php echo $row['idpartidas']?>">
-                <img class="times" src="<?php echo $varPathLocal;?>/img/times/<?php echo $row["EscudoTimeCasa"];?>" alt=""><p><?php echo $row["SiglaTimeCasa"];?></p>
+                <img class="times" src="<?php echo $varPathLocal;?>/img/times/<?php echo $row["EscudoTimeCasa"];?>" alt="">
+                <p><?php echo $row["SiglaTimeCasa"];?></p>
                 <p>X</p>
-                <p><?php echo $row["SiglaTimeVis"];?></p><img class="times" src="<?php echo $varPathLocal;?>/img/times/<?php echo $row["EscudoTimeVis"];?>" alt="">
+                <p><?php echo $row["SiglaTimeVis"];?></p>
+                <img class="times" src="<?php echo $varPathLocal;?>/img/times/<?php echo $row["EscudoTimeVis"];?>" alt="">
             </a>
             <?php
             }
+        } else {
+            echo "<p>Nenhuma partida encontrada.</p>";
         }
         ?>
-       
-        <!-- Outros jogos podem ser adicionados aqui -->
     </div>
- 
+
     <!-- Tabela da Direita com barra de rolagem na direita -->
     <div class="partidas_right">
         <div class="titulo-classificacao">TABELA DE CLASSIFICAÇÃO</div>
@@ -66,107 +64,109 @@ $data = mysqli_fetch_assoc($result);
                 <p class="info-topo">D</p>
                 <p class="info-topo">Sg</p>
             </div>
+            <!-- Adicione os itens da tabela conforme necessário -->
+            <!-- Exemplo de times adicionados -->
             <div class="time-item">
                 <p>1</p>
                 <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/BOT.png" alt="">Botafogo</div>
-                <p>70</p><p>22</p><p>4</p><p>4</p><p>30</p>
+                <p>76</p><p>22</p><p>10</p><p>5</p><p>29</p>
             </div>
             <div class="time-item">
                 <p>2</p>
                 <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/PAL.png" alt=""><p>Palmeiras</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <p>73</p><p>22</p><p>7</p><p>8</p><p>28</p>
             </div>
             <div class="time-item">
                 <p>3</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/FOR.png" alt=""><p>Fortaleza</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/FLA.png" alt=""><p>Flamengo</p></div>
+                <p>69</p><p>20</p><p>9</p><p>8</p><p>19</p>
             </div>
             <div class="time-item">
                 <p>4</p>
                 <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/inter.png" alt=""><p>Internacional</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <p>65</p><p>18</p><p>11</p><p>8</p><p>20</p>
             </div>
             <div class="time-item">
                 <p>5</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/FLA.png" alt=""><p>Flamengo</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/FOR.png" alt=""><p>Fortaleza</p></div>
+                <p>65</p><p>18</p><p>11</p><p>8</p><p>11</p>
             </div>
             <div class="time-item">
                 <p>6</p>
                 <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/SAO 2.png" alt=""><p>São Paulo</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <p>59</p><p>17</p><p>8</p><p>12</p><p>11</p>
             </div>
             <div class="time-item">
                 <p>7</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/ECB.png" alt=""><p>Bahia</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/COR.png" alt=""><p>Corinthians</p></div>
+                <p>53</p><p>14</p><p>11</p><p>12</p><p>6</p>
             </div>
             <div class="time-item">
                 <p>8</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CRU.png" alt=""><p>Cruzeiro</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/ECB.png" alt=""><p>Bahia</p></div>
+                <p>50</p><p>14</p><p>8</p><p>15</p><p>-2</p>
             </div>
             <div class="time-item">
                 <p>9</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/VAS.png" alt=""><p>Vasco</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CRU.png" alt=""><p>Cruzeiro</p></div>
+                <p>49</p><p>13</p><p>10</p><p>14</p><p>1</p>
             </div>
             <div class="time-item">
                 <p>10</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CAM.png" alt=""><p>Atlético-MG</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/VAS.png" alt=""><p>Vaco da Gama</p></div>
+                <p>47</p><p>13</p><p>8</p><p>16</p><p>-14</p>
             </div>
             <div class="time-item">
                 <p>11</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/gremio.png" alt=""><p>Grêmio</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/VIT.png" alt=""><p>Vitória</p></div>
+                <p>46</p><p>13</p><p>7</p><p>17</p><p>-7</p>
             </div>
             <div class="time-item">
                 <p>12</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/VIT.png" alt=""><p>Vitória</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/gremio.png" alt=""><p>Grêmio</p></div>
+                <p>45</p><p>12</p><p>9</p><p>16</p><p>-3</p>
             </div>
             <div class="time-item">
                 <p>13</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/COR.png" alt=""><p>Corinthians</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/JUV.png" alt=""><p>Juventude</p></div>
+                <p>45</p><p>11</p><p>12</p><p>14</p><p>-10</p>
             </div>
             <div class="time-item">
-                <p>13</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/FLU.png" alt=""><p>Fluminense</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <p>14</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CAM.png" alt=""><p>Atlético-MG</p></div>
+                <p>44</p><p>10</p><p>14</p><p>13</p><p>-8</p>
             </div>
             <div class="time-item">
                 <p>15</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CRI.png" alt=""><p>Criciúma</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/FLU.png" alt=""><p>Fluminense</p></div>
+                <p>43</p><p>11</p><p>10</p><p>16</p><p>-7</p>
             </div>
             <div class="time-item">
                 <p>16</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/RBG.png" alt=""><p>Bragantino</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CAP.png" alt=""><p>Athletico-PR</p></div>
+                <p>42</p><p>11</p><p>9</p><p>17</p><p>-5</p>
             </div>
             <div class="time-item">
                 <p>17</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CAP.png" alt=""><p>Athletico-Pr</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/RBG.png" alt=""><p>Bragantino</p></div>
+                <p>41</p><p>9</p><p>14</p><p>14</p><p>-8</p>
             </div>
             <div class="time-item">
                 <p>18</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/JUV.png" alt=""><p>Juventude</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CRI.png" alt=""><p>Criciúma</p></div>
+                <p>38</p><p>9</p><p>11</p><p>17</p><p>-15</p>
             </div>
             <div class="time-item">
                 <p>19</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CUI.png" alt=""><p>Cuiabá</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/ACG.png" alt=""><p>Atlético-GO</p></div>
+                <p>30</p><p>7</p><p>9</p><p>21</p><p>-27</p>
             </div>
             <div class="time-item">
                 <p>20</p>
-                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/ACG.png" alt=""><p>Atlético-GO</p></div>
-                <p>65</p><p>20</p><p>5</p><p>5</p><p>25</p>
+                <div class="time-logo"><img class="times" src="<?php echo $varPathLocal;?>/img/CUI.png" alt=""><p>Cuiabá</p></div>
+                <p>30</p><p>6</p><p>12</p><p>19</p><p>-19</p>
             </div>
-            <!-- Outros times da tabela podem ser adicionados aqui -->
+            <!-- Mais times podem ser adicionados aqui -->
         </div>
     </div>
 </section>
